2664891e6d2d4a5512fd8d8d5b8ad1354ea109ac
Francisco <fcocacerescolihuil@gmail.com>
2025-06-04 00:30:47 -0400

    initial commit

---
+++ src/main/java/com/example/bicyclerackapi/BicycleRackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bicyclerackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleRackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BicycleRackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bicyclerackapi/BicycleRackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bicyclerackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleRackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
