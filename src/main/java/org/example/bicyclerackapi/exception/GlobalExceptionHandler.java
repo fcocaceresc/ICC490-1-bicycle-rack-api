7340915fd33caae307536b36d037da0faeb37469
Francisco <fcocacerescolihuil@gmail.com>
2025-06-22 02:29:29 -0400

    add database connection failure exception handling and reduce timeout

--- src/main/java/org/example/bicyclerackapi/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/bicyclerackapi/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.example.bicyclerackapi.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,5 +11,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleRecordNotFoundException(RecordNotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse(404, exception.getMessage());
         return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleCannotCreateTransactionException(CannotCreateTransactionException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(503, "The database is currently unavailable. Unable to process the request.");
+        return ResponseEntity.status(503).body(errorResponse);
     }
 }
